4c24e4f8f3f311b3a6c15b1c85b7b5b417b2a5e2
unknown <Tgozali@users.noreply.github.com>
2025-07-15 15:00:40 +0300

    ver 2 - started exersice 2

--- final_exercise_kfir.py
+++ final_exercise_kfir.py
@@ -1,13 +1,26 @@
 # final exersice - movie
-def user_input():
+def user_input_movieName():
+    movie_name_input = input("type in a movie name  ")
+    return movie_name_input
+
+def user_input_movieGrade(last_movie):
     movie_name_input = input("type in a movie name  ")
     return movie_name_input
 
 def main():
-    last_movie  = user_input()
+    #this is the main func
     movie_list = []
+    end_flag = 0
 
-    movie_list.append(last_movie)
+    while end_flag == 0:
+        last_movie = user_input_movieName()
+        user_input_movieGrade(last_movie)
+        # movie_list.append(last_movie)
+
+        # print(movie_list)
+        if last_movie == "quit":
+            end_flag += 1
+
 
 if __name__ == '__main__':
     main()
